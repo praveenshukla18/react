5e700b55edb37cd18a8e5482968786ed397a0bf2
praveenshukla18 <praveenshkl06@gmail.com>
2020-05-25 16:36:13 +0530

    Add files via upload

---
+++ build/precache-manifest.d3a757c7d4471214ea6c7a6f9f0cb3e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d842af9a601266aa3955a55cca5ddd3",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "69ce0ba066ccc1b012c1",
+    "url": "/react/static/css/main.a668813a.chunk.css"
+  },
+  {
+    "revision": "024bd20c6da6362b5613",
+    "url": "/react/static/js/2.c1f8cbcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.c1f8cbcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ce0ba066ccc1b012c1",
+    "url": "/react/static/js/main.d91b9ff9.chunk.js"
+  },
+  {
+    "revision": "c8065aca78be09786158",
+    "url": "/react/static/js/runtime-main.e66c699b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
